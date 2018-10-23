741ceffde7e6d7c9ca65adcccc4dbfc396e8c647
Brian Watkins <bwatkins@Brians-MacBook-Pro.local>
2018-10-23 14:41:58 -0400

    Enable basic auth and https

---
+++ src/main/java/io/pivotal/pal/tracker/SecurityConfiguration.java
@@ -0,0 +1,40 @@
+package io.pivotal.pal.tracker;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+
+@EnableWebSecurity
+public class SecurityConfiguration extends WebSecurityConfigurerAdapter {
+
+    private Boolean httpsDisabled;
+
+    public SecurityConfiguration(@Value("${HTTPS_DISABLED}") Boolean httpsDisabled) {
+        this.httpsDisabled = httpsDisabled;
+    }
+
+    @Override
+    public void configure(HttpSecurity http) throws Exception {
+        http.authorizeRequests()
+                .anyRequest()
+                    .authenticated()
+                .and()
+                .httpBasic()
+                .and()
+                .csrf()
+                    .disable();
+
+        if (!this.httpsDisabled) {
+            http.requiresChannel().anyRequest().requiresSecure();
+        }
+    }
+
+    @Autowired
+    public void configureGlobal(AuthenticationManagerBuilder auth) throws Exception {
+        auth.inMemoryAuthentication()
+                .withUser("user").password("password").roles("USER");
+    }
+}
